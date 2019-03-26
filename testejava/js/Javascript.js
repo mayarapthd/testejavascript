0c02269333ddb9861bc43d8258e7806ffe10a7fa
mayarapthd <mayaratheodoro@outlook.com.br>
2019-03-25 22:51:32 -0300

    aula25022019

---
+++ testejava/js/Javascript.js
@@ -0,0 +1,18 @@
+function trocaImagem1 () {
+		document.getElementById ("myImage").src="img/html-logo-badge-512.png";
+}
+
+function imagemTroca () {
+		document.getElementById("myImage").src="img/js-logo-badge-512.png";
+}
+
+function trocaImagem2 () {
+		document.getElementById ("myImage").src="img/js-logo-badge-512.png";
+}
+
+function imagemTroca () {
+		document.getElementById("myImage").src="img/html-logo-badge-512.png";
+}
+
+
+
